13c99a5254b5d9db0a317b1f6eb780e3d3312051
Panagiotis Savvaidis <68665285+psavvaidis@users.noreply.github.com>
2022-11-11 03:52:45 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/95630f79-9578-4929-9832-db984ec76979/guestbook/';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/95630f79-9578-4929-9832-db984ec76979/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
